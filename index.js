3980962504725be4db798def05435dd1a2b77bb6
Xotic750 <xotic750@gmail.com>
2017-07-11 17:17:23 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,62 +1,33 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/is-array-like-x"
- * title="Travis status">
- * <img src="https://travis-ci.org/Xotic750/is-array-like-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/is-array-like-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/is-array-like-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a href="https://david-dm.org/Xotic750/is-array-like-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/is-array-like-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/is-array-like-x" title="npm version">
- * <img src="https://badge.fury.io/js/is-array-like-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * ES6 isArrayLike module.
- *
- * @version 1.2.0
+ * @file Determine if a value is array like.
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module is-array-like-x
  */
 
-/* eslint strict: 1 */
+'use strict';
 
-/* global module */
+var isNil = require('is-nil-x');
+var isFunction = require('is-function-x');
+var isLength = require('lodash.islength');
 
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var isNil = require('is-nil-x');
-  var isFunction = require('is-function-x');
-  var isLength = require('lodash.islength');
-
-  /**
-   * Checks if value is array-like. A value is considered array-like if it's
-   * not a function and has a `length` that's an integer greater than or
-   * equal to 0 and less than or equal to `Number.MAX_SAFE_INTEGER`.
-   *
-   * @param {*} value The object to be tested.
-   * @return {boolean} Returns `true` if subject is array-like, else `false`.
-   * @example
-   * var isArrayLike = require('is-array-like-x');
-   *
-   * isArrayLike([1, 2, 3]); // true
-   * isArrayLike(document.body.children); // true
-   * isArrayLike('abc'); // true
-   * isArrayLike(_.noop); // false
-   */
-  module.exports = function isArrayLike(value) {
-    return !isNil(value) && !isFunction(value) && isLength(value.length);
-  };
-}());
+/**
+ * Checks if value is array-like. A value is considered array-like if it's
+ * not a function and has a `length` that's an integer greater than or
+ * equal to 0 and less than or equal to `Number.MAX_SAFE_INTEGER`.
+ *
+ * @param {*} value - The object to be tested.
+ * @returns {boolean} Returns `true` if subject is array-like, else `false`.
+ * @example
+ * var isArrayLike = require('is-array-like-x');
+ *
+ * isArrayLike([1, 2, 3]); // true
+ * isArrayLike(document.body.children); // true
+ * isArrayLike('abc'); // true
+ * isArrayLike(_.noop); // false
+ */
+module.exports = function isArrayLike(value) {
+  return isNil(value) === false && isFunction(value) === false && isLength(value.length);
+};
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,98 +1,97 @@
-/* jslint maxlen:80, es6:true, white:true */
+'use strict';
 
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:true, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
-   maxstatements:11, maxcomplexity:3 */
-
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1, no-sparse-arrays: 1, no-new-func: 1 */
-
-/* global JSON:true, expect, module, require, describe, it, returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var isArrayLike;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    var es7 = require('es7-shim');
-    Object.keys(es7).forEach(function (key) {
-      var obj = es7[key];
-      if (typeof obj.shim === 'function') {
-        obj.shim();
-      }
-    });
-    isArrayLike = require('../../index.js');
-  } else {
-    isArrayLike = returnExports;
+var isArrayLike;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
-
-  describe('Basic tests', function () {
-    it('should return `true` for array-like values', function () {
-      (function () {
-        var values = [arguments, [1, 2, 3], { 0: 1, length: 1 }, 'a'],
-          expected = values.map(function () {
-            return true;
-          }),
-          actual = values.map(isArrayLike);
-        expect(actual).toEqual(expected);
-      }(1, 2, 3));
-    });
-
-    it('should return `false` for non-arrays', function () {
-      /* jshint elision:true */
-      var falsey = [, '', 0, false, NaN, null, undefined];
-      var generator = (function () {
-        try {
-            /* jshint evil:true */
-          return new Function('return function*(){}');
-        } catch (ignore) {
-          return void 0;
-        }
-      }());
-      var expected = falsey.map(function (value) {
-        return value === '';
-      });
-      var actual = falsey.map(function (value, index) {
-        return index ? isArrayLike(value) : isArrayLike();
-      });
-      expect(actual).toEqual(expected);
-      expect(isArrayLike(true)).toBe(false);
-      expect(isArrayLike(new Date())).toBe(false);
-      expect(isArrayLike(new Error())).toBe(false);
-      expect(isArrayLike({})).toBe(false);
-      expect(isArrayLike(generator)).toBe(false);
-      expect(isArrayLike(Array.prototype.slice)).toBe(false);
-      expect(isArrayLike({ a: 1 })).toBe(false);
-      expect(isArrayLike(1)).toBe(false);
-      expect(isArrayLike(/x/)).toBe(false);
-
-      try {
-        /* jshint evil:true */
-        var fat = new Function('return () => {return this;}');
-        expect(isArrayLike(fat)).toBe(false);
-      } catch (ignore) {}
-
-      try {
-        /* jshint evil:true */
-        var gen = new Function('return function* idMaker(){}');
-        expect(isArrayLike(gen)).toBe(false);
-      } catch (ignore) {}
-
-      try {
-        /* jshint evil:true */
-        var classes = new Function('"use strict"; return class MyClass {}');
-        expect(isArrayLike(classes)).toBe(false);
-      } catch (ignore) {}
-    });
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  var es7 = require('es7-shim');
+  Object.keys(es7).forEach(function (key) {
+    var obj = es7[key];
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
   });
-}());
+  isArrayLike = require('../../index.js');
+} else {
+  isArrayLike = returnExports;
+}
+
+describe('Basic tests', function () {
+  it('should return `true` for array-like values', function () {
+    (function () {
+      var values = [
+        arguments,
+        [
+          1,
+          2,
+          3
+        ],
+        { 0: 1, length: 1 },
+        'a'
+      ];
+      var expected = values.map(function () {
+        return true;
+      });
+      var actual = values.map(isArrayLike);
+      expect(actual).toEqual(expected);
+    }(1, 2, 3));
+  });
+
+  it('should return `false` for non-arrays', function () {
+    var falsey = [
+      '',
+      0,
+      false,
+      NaN,
+      null,
+      undefined
+    ];
+    var generator = (function () {
+      try {
+        // eslint-disable-next-line no-new-func
+        return new Function('return function*(){}');
+      } catch (ignore) {
+        return void 0;
+      }
+    }());
+    var expected = falsey.map(function (value) {
+      return value === '';
+    });
+    var actual = falsey.map(function (value) {
+      return isArrayLike(value);
+    });
+    expect(actual).toEqual(expected);
+    expect(isArrayLike(true)).toBe(false);
+    expect(isArrayLike(new Date())).toBe(false);
+    expect(isArrayLike(new Error())).toBe(false);
+    expect(isArrayLike({})).toBe(false);
+    expect(isArrayLike(generator)).toBe(false);
+    expect(isArrayLike(Array.prototype.slice)).toBe(false);
+    expect(isArrayLike({ a: 1 })).toBe(false);
+    expect(isArrayLike(1)).toBe(false);
+    expect(isArrayLike(/x/)).toBe(false);
+
+    try {
+      // eslint-disable-next-line no-new-func
+      var fat = new Function('return () => {return this;}');
+      expect(isArrayLike(fat)).toBe(false);
+    } catch (ignore) {}
+
+    try {
+      // eslint-disable-next-line no-new-func
+      var gen = new Function('return function* idMaker(){}');
+      expect(isArrayLike(gen)).toBe(false);
+    } catch (ignore) {}
+
+    try {
+      // eslint-disable-next-line no-new-func
+      var classes = new Function('"use strict"; return class MyClass {}');
+      expect(isArrayLike(classes)).toBe(false);
+    } catch (ignore) {}
+  });
+});
