0d2ec350a902cde12797ae1f3cd9c12ae9c8f5ab
Graham Fairweather <xotic750@gmail.com>
2019-08-20 15:25:57 +0200

    Maintenance

--- dist/is-array-like-x.js
+++ dist/is-array-like-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-20T09:31:26.012Z",
+  "date": "2019-08-20T13:25:50.917Z",
   "describe": "",
   "description": "Determine if a value is array like.",
   "file": "is-array-like-x.js",
